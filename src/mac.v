26d4161ae9c9183305366a70af2845e7bc453162
Mikko Manninen <mikko@manninen.com>
2021-09-30 18:03:55 +0300

    Initial implementation of mac.v

--- src/mac.v
+++ src/mac.v
@@ -1,10 +1,19 @@
 // Test Verilog module
 module mac (
-    output     reg[0:15] out,
-    input       reg[0:15] ina, inb, 
-    input       clk, sclrn
+    output reg[15:0]    out,
+    input[7:0]              ina, inb, 
+    input                     clk, sclrn
 );
 
+task mult;
+    input[7:0] a, b;
+    output[15:0] mult_out;
+begin
+    mult_out = a * b;
+end
+endtask
 
+always @*
+      mult(ina, inb, out);
 
 endmodule
