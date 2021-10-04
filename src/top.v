b26e20dc381804d40649901fd1a05f106c1b510b
Mikko Manninen <mikko@manninen.com>
2021-10-04 13:36:45 +0300

    Added top level module!

---
+++ src/top.v
@@ -0,0 +1,21 @@
+module top (clk, SCK, MOSI, MISO, SSEL, LED);
+
+input clk; // FPGA internal clk from crystal on board
+
+// 4 wire SPI signals
+input SCK, SSEL, MOSI;  
+output MISO;
+
+// Controlled output for demo
+output LED;
+
+
+// Instantiate alternative clock, the internal osc
+Gowin_OSC FPGA_osc(
+    .oscout(oscout_o) //output oscout
+);
+
+// Instantiate and connect the SPI block
+ SPI_slave(oscout_o, SCK, MOSI, MISO, SSEL, LED);
+
+endmodule
